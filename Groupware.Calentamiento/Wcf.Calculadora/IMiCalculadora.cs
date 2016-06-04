ed1dfec5de7a9017ba3ef4a706872374cfe0ed05
nana1717 <nachamocr17@gmail.com>
2016-06-04 15:24:37 -0600

    VersionClase4

--- Groupware.Calentamiento/Wcf.Calculadora/IMiCalculadora.cs
+++ Groupware.Calentamiento/Wcf.Calculadora/IMiCalculadora.cs
@@ -26,8 +26,10 @@ namespace Wcf.Calculadora
     }
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
+
+
+// Use a data contract as illustrated in the sample below to add composite types to service operations.
+[DataContract]
     public class CompositeType
     {
         bool boolValue = true;
