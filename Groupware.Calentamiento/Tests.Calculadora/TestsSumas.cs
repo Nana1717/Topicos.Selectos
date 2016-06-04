914f70140666da5bbf61e776bba345e66faf5a37
nana1717 <nachamocr17@gmail.com>
2016-06-04 16:03:58 -0600

    VersionClase4

--- Groupware.Calentamiento/Tests.Calculadora/TestsSumas.cs
+++ Groupware.Calentamiento/Tests.Calculadora/TestsSumas.cs
@@ -24,5 +24,23 @@ namespace Tests.Calculadora
             //Verificar si el resultado obtenido es el mismo que el esperado
             Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
         }
+
+        [TestMethod]
+        public void SumaConDecimales()
+        {
+            //Definicion del escenario
+            Numero elPrimerOperando = new Numero("1.3", 10);
+            Numero elSegundoOperando = new Numero("-9.5", 10);
+            char elOperador = '+';
+            Numero elResultadoEsperado = new Numero("-8.2", 10);
+            Numero elResultadoReal;
+
+            //Invoque al metodo que se prueba
+            var laReferencia = new Core.Numero.Dominio.Acciones.Suma();
+            elResultadoReal = laReferencia.Operar(elPrimerOperando, elSegundoOperando);
+
+            //Verificar si el resultado obtenido es el mismo que el esperado
+            Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
+        }
     }
 }
